c68bffc503938ee44b75dacce3badfbc47bab420
jingairpi <jingairpi@gmail.com>
2026-05-15 17:48:36 -0700

    feat: add the embedding layer

--- tests/adapters.py
+++ tests/adapters.py
@@ -12,6 +12,7 @@ from torch import Tensor
 from cs336_basics.bpe import BPETokenizer
 from cs336_basics.train_bpe import train_bpe
 from cs336_basics.linear import Linear
+from cs336_basics.embedding import Embedding
 
 
 def run_linear(
@@ -57,7 +58,9 @@ def run_embedding(
         Float[Tensor, "... d_model"]: Batch of embeddings returned by your Embedding layer.
     """
 
-    raise NotImplementedError
+    embedding = Embedding(vocab_size, d_model)
+    embedding.weight.data = weights
+    return embedding(token_ids)
 
 
 def run_swiglu(
