6aac5e27f17aede2f127b657d8bfede27643c55d
jingairpi <jingairpi@gmail.com>
2026-05-15 17:51:33 -0700

    chore: refine the linear module

--- cs336_basics/linear.py
+++ cs336_basics/linear.py
@@ -16,10 +16,8 @@ class Linear(nn.Module):
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.device = device
-        self.dtype = dtype
-        self.weight = nn.Parameter(torch.zeros(self.out_features, self.in_features, device=device, dtype=dtype))
-        nn.init.trunc_normal_(self.weight, mean=0.0, std=math.sqrt(2 / (self.in_features + self.out_features)))
+        self.weight = nn.Parameter(torch.zeros(out_features, in_features, device=device, dtype=dtype))
+        nn.init.trunc_normal_(self.weight, mean=0.0, std=math.sqrt(2 / (in_features + out_features)))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return einsum(x, self.weight, "... d_in, d_out d_in -> ... d_out")
