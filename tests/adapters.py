cd0a2053637a32e0043ea5446a448e63d72dd77c
jingairpi <jingairpi@gmail.com>
2026-05-15 20:40:00 -0700

    feat: Implement RMSNorm module

--- tests/adapters.py
+++ tests/adapters.py
@@ -13,6 +13,7 @@ from cs336_basics.bpe import BPETokenizer
 from cs336_basics.train_bpe import train_bpe
 from cs336_basics.linear import Linear
 from cs336_basics.embedding import Embedding
+from cs336_basics.rmsnorm import RMSNorm
 
 
 def run_linear(
@@ -387,7 +388,10 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+
+    rms_norm = RMSNorm(d_model, eps)
+    rms_norm.weight.data = weights
+    return rms_norm(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
