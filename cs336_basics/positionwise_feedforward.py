fbec89c85fdafb5505a3033104825a4578ad4ca9
jingairpi <jingairpi@gmail.com>
2026-05-15 23:07:13 -0700

    chore: fix SWiGLU module init

--- cs336_basics/positionwise_feedforward.py
+++ cs336_basics/positionwise_feedforward.py
@@ -15,9 +15,9 @@ class SwiGLU(nn.Module):
         super().__init__()
         self.d_model = d_model
         self.d_ff = d_ff
-        self.w1 = Linear(d_model, d_ff)
-        self.w2 = Linear(d_ff, d_model)
-        self.w3 = Linear(d_model, d_ff)
+        self.w1 = Linear(d_model, d_ff, device, dtype)
+        self.w2 = Linear(d_ff, d_model, device, dtype)
+        self.w3 = Linear(d_model, d_ff, device, dtype)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         w1_out = self.w1(x)
