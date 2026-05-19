ea70fcd0c61f543cbdb6ca4918760dcfd3c84a30
jingairpi <42414856+jingairpi@users.noreply.github.com>
2026-05-19 00:29:08 -0700

    feat: implement lr cosine schedule (#22)

--- tests/adapters.py
+++ tests/adapters.py
@@ -23,6 +23,7 @@ from cs336_basics.transformer_block import TransformerBlock
 from cs336_basics.transformer_lm import TransformerLM
 from cs336_basics.cross_entropy import cross_entropy
 from cs336_basics.adamw import AdamW
+from cs336_basics.learning_rate_schedule import get_lr_cosine_schedule
 
 
 def run_linear(
@@ -568,7 +569,8 @@ def run_get_lr_cosine_schedule(
     Returns:
         Learning rate at the given iteration under the specified schedule.
     """
-    raise NotImplementedError
+
+    return get_lr_cosine_schedule(it, max_learning_rate, min_learning_rate, warmup_iters, cosine_cycle_iters)
 
 
 def run_save_checkpoint(
